efaf1ea2dfb9962acd023eaf98d5c4fa692521de
warmhand <kevinlee8817@gmail.com>
2025-11-28 15:28:50 +0900

    add bar with text ver2

--- app/src/main/java/com/example/aborea/pages/statistics/compose/BarWithText.kt
+++ app/src/main/java/com/example/aborea/pages/statistics/compose/BarWithText.kt
@@ -1,3 +1,13 @@
 package com.example.aborea.pages.statistics.compose
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.runtime.Composable
 import com.example.aborea.common.*
+
+@Composable
+fun BarWithText(text: String, color: Long) {
+    Row {
+        OwnglyphText(text, 30, 0, 0, 0xFF6A6A6A)
+        ProgressBar(0.4f, color)
+    }
+}
